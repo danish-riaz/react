ddb7c349c83afe9e8b9319846c430b716fa3c6f9
Danish Riaz <danishriaz0609@outlook.com>
2020-06-20 11:42:50 +0500

    class component

--- 01_basics_monsters_rolodex_app/src/App.js
+++ 01_basics_monsters_rolodex_app/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Danish Riaz',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.name}</p>
+          <button onClick={() => this.setState({ name: 'Your Name' })}>
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
