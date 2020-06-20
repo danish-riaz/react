9bce52090f8d62007bb6d3188a64d6e555680183
Danish Riaz <danishriaz0609@outlook.com>
2020-06-20 20:30:45 +0500

    card-list component

--- 01_basics_monsters_rolodex_app/src/App.js
+++ 01_basics_monsters_rolodex_app/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -20,9 +23,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList>
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
