63457b2338b5654dbc2ff88101777dce68b3927b
Danish Riaz <danishriaz0609@outlook.com>
2020-06-22 12:27:07 +0500

    added base project, setup directory structor, and added directory

---
+++ 03_master_project_setting_up_e-commerce_project/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HomePage from './pages/home-page/home-page.component';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
