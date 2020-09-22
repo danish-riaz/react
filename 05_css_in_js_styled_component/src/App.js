de92c2557efdc4ddb46d96c4c75d19ebc7d32382
Danish Riaz <danishriaz0609@outlook.com>
2020-09-22 11:28:51 +0500

    styled-components

--- 05_css_in_js_styled_component/src/App.js
+++ 05_css_in_js_styled_component/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  &:hover {
+    color: blue;
+    background-color: green;
+  }
+  width: 40vw;
+  height: 100px;
+  margin: 80px auto;
+  line-height: 100px;
+  color: red;
+  font-family: sans;
+  background-color: pink;
+  text-align: center;
+  border-radius: 32px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <H1>Styled Components</H1>
     </div>
   );
 }
